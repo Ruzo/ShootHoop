96f9c2c16ca4b93ffdced05ab8da2fb909831ffc
Ruzo <richard@evigy.com>
2016-04-19 22:35:55 -0400

    Up to shooting the ball forward from the camera's position.

---
+++ ShootHoop/Assets/Scripts/BallShooter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShooter : MonoBehaviour {
+
+    public GameObject ballSpawn;
+    public float ballSpeed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootBall();
+        }
+    }
+
+    private void ShootBall()
+    {
+        GameObject instance = Instantiate(ballSpawn);
+        Transform transf = instance.GetComponent<Transform>();
+        Rigidbody rb = instance.GetComponent<Rigidbody>();
+        Transform camera = transform.Find("Camera");
+        transf.position = camera.transform.position;
+        Vector3 direction = camera.transform.forward;
+        rb.velocity = direction * ballSpeed;
+    }
+}
---
+++ ShootHoop/Assets/Scripts/ViewRotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewRotator : MonoBehaviour {
+
+    const float MOUSE_SENSITIVITY = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X") * MOUSE_SENSITIVITY;
+        float mouseY = Input.GetAxis("Mouse Y") * MOUSE_SENSITIVITY;
+        Transform camera = transform.Find("Camera");
+        // Another way: Camera camera = GetComponentInChildren<Camera>();
+        camera.localRotation = Quaternion.Euler(-mouseY, 0, 0) * camera.localRotation;
+        transform.rotation = Quaternion.Euler(0, mouseX, 0) * transform.rotation;
+    }
+}
